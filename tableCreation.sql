34d9cc8ba89fd3440cb335b582694b74e623c950
Sanya Korovkina <127419480+sasha-korovkina@users.noreply.github.com>
2023-10-31 09:45:08 +0000

    Update tableCreation.sql

--- tableCreation.sql
+++ tableCreation.sql
@@ -5,62 +5,73 @@
 alter PROCEDURE dbo.createColumns
 AS
 BEGIN
-
-    ALTER TABLE publicEquitiesWorking
+	
+    ALTER TABLE publicEquitiesRawCopy
     ADD [isin] VARCHAR(255);
 
-    ALTER TABLE publicEquitiesWorking
+    ALTER TABLE publicEquitiesRawCopy
     ADD [note] VARCHAR(255);
 
-    ALTER TABLE publicEquitiesWorking
+    ALTER TABLE publicEquitiesRawCopy
     ADD [non_disclosed] INT;
 
-    ALTER TABLE publicEquitiesWorking
+    ALTER TABLE publicEquitiesRawCopy
     ADD [grey_area] INT;
 
-    ALTER TABLE publicEquitiesWorking
+    ALTER TABLE publicEquitiesRawCopy
     ADD [vlref] VARCHAR(255);
 
-	ALTER TABLE publicEquitiesWorking
+	ALTER TABLE publicEquitiesRawCopy
     ADD [holding_date] datetime;
 
-	ALTER TABLE publicEquitiesWorking
+	ALTER TABLE publicEquitiesRawCopy
     ADD [disclosure_source_key] INT;
 
-	ALTER TABLE publicEquitiesWorking
+	ALTER TABLE publicEquitiesRawCopy
     ADD [client_code] VARCHAR(255);
 
-	ALTER TABLE publicEquitiesWorking
+	ALTER TABLE publicEquitiesRawCopy
     ADD [project_code] VARCHAR(255);
 
-	ALTER TABLE publicEquitiesWorking
+	ALTER TABLE publicEquitiesRawCopy
     ADD [listing] VARCHAR(255);
 END;
 
 alter PROCEDURE dbo.updateColumns
 AS
-	UPDATE publicEquitiesWorking
+	UPDATE publicEquitiesRawCopy
 	SET listing = 'ORD';
 
-	UPDATE publicEquitiesWorking
+	UPDATE publicEquitiesRawCopy
 	SET holding_date = getdate();
 
-	UPDATE publicEquitiesWorking
+	UPDATE publicEquitiesRawCopy
 	SET disclosure_source_key = 69;
 
-	UPDATE publicEquitiesWorking
+	UPDATE publicEquitiesRawCopy
 	SET client_code = (
-		SELECT DISTINCT clientCode COLLATE Latin1_General_CI_AI
-		FROM snd.dbo.publicEquitiesTest
+		SELECT [client_code]
+		FROM [CDB].[ent].[securities]
+		WHERE TICKER COLLATE Latin1_General_CI_AS = (SELECT distinct clientCode COLLATE Latin1_General_CI_AS FROM publicEquitiesRaw)
 	);
 
-	UPDATE publicEquitiesWorking
+	ALTER TABLE publicEquitiesRawCopy
+	DROP COLUMN clientCode;
+	
+	UPDATE t
+	SET t.note = d.note
+	FROM publicEquitiesRawCopy AS t
+	JOIN [CDB].[ent].[entity] AS e
+	ON t.EntityID COLLATE Latin1_General_CI_AS = e.cmi2i_code COLLATE Latin1_General_CI_AS
+	JOIN [CDB].[own].[disclosed] d ON e.ent_id = d.fund_ent_id
+
+	UPDATE publicEquitiesRawCopy
 	SET project_code = (
 		SELECT max(project_code)
 		FROM [CDB].[ent].[project]
 		where client_code = 'VES');
 
-	UPDATE publicEquitiesWorking
+	UPDATE publicEquitiesRawCopy
 	SET listing = (
 	SELECT listing
 	FROM [CDB].[ent].[securities] s
@@ -68,14 +79,19 @@ AS
 	WHERE ticker = (
 		SELECT DISTINCT clientCode COLLATE Latin1_General_CI_AI
 		FROM snd.dbo.publicEquitiesTest
-	))
+	)
+)
 
-CREATE PROCEDURE dbo.publicToSQL
+ALTER PROCEDURE dbo.publicToSQL
 AS
 BEGIN
+	SELECT *
+	INTO publicEquitiesRawCopy
+	FROM publicEquitiesRaw;
+
     EXEC dbo.createColumns;
     EXEC dbo.updateColumns;
 END;
 
-select * from publicEquitiesWorking
-
+exec publicToSQL
+select * from publicEquitiesRawCopy
