d0562c530d304f9ccf46f40a5988f25d0af414cd
Sanya Korovkina <127419480+sasha-korovkina@users.noreply.github.com>
2023-10-31 11:16:55 +0000

    Update tableCreation.sql

--- tableCreation.sql
+++ tableCreation.sql
@@ -41,13 +41,14 @@ BEGIN
     ADD [project_code] VARCHAR(255);
 
 	ALTER TABLE publicEquitiesRawCopy
-    ADD [listing] VARCHAR(255);
+    ADD [listing_key] VARCHAR(255);
+
+	ALTER TABLE publicEquitiesRawCopy
+    ADD mapped_user VARCHAR(255);
 END;
 
 alter PROCEDURE dbo.updateColumns
 AS
-	UPDATE publicEquitiesRawCopy
-	SET listing = 'ORD';
 
 	UPDATE publicEquitiesRawCopy
 	SET holding_date = getdate();
@@ -56,11 +57,20 @@ AS
 	SET disclosure_source_key = 69;
 
 	UPDATE publicEquitiesRawCopy
+	SET mapped_user = 'cmds\laura';
+
+	UPDATE publicEquitiesRawCopy
 	SET client_code = (
 		SELECT [client_code]
 		FROM [CDB].[ent].[securities]
 		WHERE TICKER COLLATE Latin1_General_CI_AS = (SELECT distinct clientCode COLLATE Latin1_General_CI_AS FROM publicEquitiesRaw)
 	);
+
+	UPDATE publicEquitiesRawCopy
+	SET listing_key = (select listing_key from [CDB].[ent].[securities] 
+	where ticker = 
+		(SELECT DISTINCT clientCode COLLATE Latin1_General_CI_AI
+		FROM snd.dbo.publicEquitiesRaw));
 
 	ALTER TABLE publicEquitiesRawCopy
 	DROP COLUMN clientCode;
@@ -78,16 +88,7 @@ AS
 		FROM [CDB].[ent].[project]
 		where client_code = 'VES');
 
-	UPDATE publicEquitiesRawCopy
-	SET listing = (
-	SELECT listing
-	FROM [CDB].[ent].[securities] s
-	JOIN cdb.ref.listing_key l on s.listing_key = l.listing_key
-	WHERE ticker = (
-		SELECT DISTINCT clientCode COLLATE Latin1_General_CI_AI
-		FROM snd.dbo.publicEquitiesTest
-	)
-)
+
 
 ALTER PROCEDURE dbo.publicToSQL
 AS
@@ -118,4 +119,79 @@ BEGIN
 END;
 
 exec publicToSQL
-select * from publicEquitiesRawCopy
+
+--------------------------------------------- PREPPING FOR OWN DISCLOSED ---------------------------------------------
+
+select * 
+from publicEquitiesRawCopy
+
+select 
+	case when e.database_key = 'I' then e.ent_id else null end as advisor_ent_id
+	, case when e.database_key = 'F' then e.ent_id else null end as fund_ent_id
+	, client_code
+	, project_code
+	, listing_key 
+	, [disclosure_source_key]
+	, [vlref]
+	, [holding_date]
+	, [position]
+	, [grey_area]
+	, [researchDate] as research
+	, [note]
+	, [non_disclosed]
+	, isin
+	, c.[mapped_user]
+	, [mapped_datetime]
+FROM publicEquitiesRawCopy c
+LEFT JOIN [CDB].[map].[code] AS co
+ON c.entityID COLLATE Latin1_General_CI_AS = co.code 
+LEFT JOIN [CDB].ent.entity AS e
+ON e.ent_id = co.ent_id 
+where created_user = 'factset'
+
+CREATE PROCEDURE InsertIntoDisclosedTable
+AS
+BEGIN
+    INSERT INTO [CDB].[own].[disclosed] (
+        advisor_ent_id,
+        fund_ent_id,
+        client_code,
+        project_code,
+        listing_key,
+        [disclosure_source_key],
+        [vlref],
+        [holding_date],
+        [position],
+        [grey_area],
+        research,
+        [note],
+        [non_disclosed],
+        isin,
+        [mapped_user],
+        [mapped_datetime]
+    )
+    SELECT 
+        CASE WHEN e.database_key = 'I' THEN e.ent_id ELSE NULL END AS advisor_ent_id,
+        CASE WHEN e.database_key = 'F' THEN e.ent_id ELSE NULL END AS fund_ent_id,
+        client_code,
+        project_code,
+        listing_key,
+        [disclosure_source_key],
+        [vlref],
+        [holding_date],
+        [position],
+        [grey_area],
+        [researchDate] as research,
+        [note],
+        [non_disclosed],
+        isin,
+        c.[mapped_user],
+        [mapped_datetime]
+    FROM publicEquitiesRawCopy c
+    LEFT JOIN [CDB].[map].[code] AS co ON c.entityID COLLATE Latin1_General_CI_AS = co.code
+    LEFT JOIN [CDB].ent.entity AS e ON e.ent_id = co.ent_id
+    WHERE created_user = 'factset';
+END;
+
+
+EXEC InsertIntoDisclosedTable;
